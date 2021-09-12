cdc80a5a8974342fb269a7abc839402ac90a6e80
kbanchi1111 <kbanchi.1111@gmail.com>
2021-09-12 13:52:54 +0900

    add language controller

---
+++ app/Http/Controllers/LanguageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use PhpParser\Node\Expr\FuncCall;
+
+class LanguageController extends Controller
+{
+    public function index()
+    {
+        $languages = ['HTML', 'CSS', 'JavaScript'];
+        $data = [
+            'languages' => $languages,
+        ];
+        return view('language.index', $data);
+    }
+}
